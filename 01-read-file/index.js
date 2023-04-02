665315514b691ce2084380b86da713d4f2b66691
romanchuchev <romanok20@gmail.com>
2023-04-02 12:32:47 +0300

    feat: add read and write file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const { log, error } = require("console");
 
-let text = fs.readFileSync(path.join("01-read-file", "text.txt"), "utf8");
-console.log(text);
+const stream = new fs.ReadStream(path.join(__dirname, "text.txt"), "utf-8");
+
+stream.on("data", (chunk) => log(chunk));
+
+stream.on("error", (err) =>
+  err.code === "ENOENT" ? log("Файл не найден") : error(err)
+);
